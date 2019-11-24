f546b62baf21d0a640d9434decd891702b5e52fb
Fito Toledano <fito@peepteam.com>
2019-11-24 04:51:45 +0100

    Summary
    Initial Commit

--- Kindle/AppDelegate.swift
+++ Kindle/AppDelegate.swift
@@ -11,10 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // We set a different starting point for the app
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController()
+        
         return true
     }
 
--- Kindle/ViewController.swift
+++ Kindle/ViewController.swift
@@ -32,22 +32,7 @@ class Book {
 
 class ViewController: UIViewController {
 
-    func setupPages(){
-        let page1 = Page(number: 1, body: "This is Page1")
-        let page2 = Page(number: 2, body: "This is Page2")
-        
-        let pages = [page1, page2]
-        
-        setupBook(withPages: pages)
-    }
-    
-    func setupBook(withPages pages:[Page]){
-        let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
-
-        for page in book.pages {
-            print(page.text)
-        }
-    }
+    var books: [Book]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,6 +40,51 @@ class ViewController: UIViewController {
         
         setupPages()
     }
+    
+    func setupPages(){
+        let page1 = Page(number: 1, body: "This is Page1")
+        let page2 = Page(number: 2, body: "This is Page2")
+        let page3 = Page(number: 3, body: "This is Page3")
+        let page4 = Page(number: 4, body: "This is Page4")
+        
+        let pages = [page1, page2, page3, page4]
+        
+        setupBook(withPages: pages)
+    }
+    
+    func setupBook(withPages pages:[Page]){
+        
+        let sjobsPages = [pages[0], pages[1]]
+        let gatesPages = [pages[2], pages[3]]
+        
+        let book1 = Book(
+            title: "Steve Jobs",
+            author: "Walter Isaacson",
+            pages: sjobsPages)
+
+        let book2 = Book(title: "Bill Gates: A Biography",
+                         author: "Michael Becraft",
+                         pages: gatesPages)
+        
+        books = [book1, book2]
+                
+        // Safely unwrapping books without using the ! operator.
+        if let safeBooks = books {
+            for book in safeBooks  {
+                print("Title: \(book.title), by: \(book.author)")
+                print("Number of pages: \(pages.count)")
+                
+                for page in book.pages {
+                    print("Page #\(page.number)")
+                    print(page.text)
+                }
+                print("End of the book")
+            }
+        }
+
+    }
+    
+    
 
 
 }
