42b4cc4d665d35ae1f4e3c62acbfdec8edebc9e1
Fito Toledano <fito@peepteam.com>
2019-11-24 23:39:26 +0100

    Setting up CollectionViewController for book pages

---
+++ Kindle/Pages/PageCell.swift
@@ -0,0 +1,19 @@
+//
+//  PageCell.swift
+//  Kindle
+//
+//  Created by Fito Toledano on 11/24/19.
+//  Copyright © 2019 PEEP TECHNOLOGIES SL. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Kindle/Pages/PagesCollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PageCollectionViewControllr.swift
+//  Kindle
+//
+//  Created by Fito Toledano on 11/24/19.
+//  Copyright © 2019 PEEP TECHNOLOGIES SL. All rights reserved.
+//
+
+import UIKit
+
+class PagesCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    
+}
